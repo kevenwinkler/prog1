665998e9ee7d5548f9addb97a6daf7c1a5bbb61c
Tobias Stein <tobias.stein@tu-bs.de>
2014-01-18 18:47:07 +0100

    kleinere Dinge verändert, die checkstyle bemängelt hat

--- exercise4/Funktion.java
+++ exercise4/Funktion.java
@@ -3,7 +3,7 @@
  * 
 */
 public abstract class Funktion {
-	/**
+    /**
      * @param args wird nicht verwendet
      */
     double x, eps;
@@ -15,7 +15,7 @@ public abstract class Funktion {
     static Funktion[] rechnen = new Funktion[10];
     
     public void tabelle() {
-    	int xmax = 130;
+        int xmax = 130;
         System.out.println();
         System.out.println("WERTETABELLE");
         do {
@@ -29,11 +29,11 @@ public abstract class Funktion {
     }
     
     public void newton(double s) {
-    	this.x = s;
+        this.x = s;
         System.out.println();
         System.out.println("Startwert: " + this.x);
         do {
-        	this.x = this.x - this.f() / this.g();
+            this.x = this.x - this.f() / this.g();
             System.out.printf("x:%12.8f", this.x);
             System.out.printf("  |  y:%12.8f\n", this.f());
             this.i++;
@@ -47,26 +47,26 @@ public abstract class Funktion {
     } 
     
     public static void main(String[] args) {
-    	// Tabelle/Parabel (x-Wert[= Startwert}, eps, a, b, c, d)
-    	rechnen[0] = new Tabelle(1.0 , 0.00000001 , 2 , -20 , -6, 30);
-    	rechnen[0].newton(1.0);
-    	rechnen[1] = new Tabelle(-2.0 , 0.1 , 2.0 , -20.0 , -6.0, 30.0);
-    	rechnen[1].tabelle();
-    	rechnen[2] = new Parabel(0.5 , 0.00000001 , 1.0 , 3.0 , -5.0);
-    	rechnen[2].newton(0.5);
-    	rechnen[3] = new Parabel(0.5 , 0.1 , 1.0 , 3.0 , -5.0);
-    	rechnen[3].tabelle();
+        // Tabelle/Parabel (x-Wert[= Startwert}, eps, a, b, c, d)
+        rechnen[0] = new Tabelle(1.0 , 0.00000001 , 2 , -20 , -6, 30);
+        rechnen[0].newton(1.0);
+        rechnen[1] = new Tabelle(-2.0 , 0.1 , 2.0 , -20.0 , -6.0, 30.0);
+        rechnen[1].tabelle();
+        rechnen[2] = new Parabel(0.5 , 0.00000001 , 1.0 , 3.0 , -5.0);
+        rechnen[2].newton(0.5);
+        rechnen[3] = new Parabel(0.5 , 0.1 , 1.0 , 3.0 , -5.0);
+        rechnen[3].tabelle();
         /* 
          * Wertetabelle hat einen Vorzeichenwechsel bei -1.3 auf -1.2 -> Nullstelle
            Wertetabelle hat einen Vorzeichenwechsel bei 1.1 auf 1.2 -> Nullstelle
            Wertetabelle hat einen Vorzeichenwechsel bei 10.1 auf 10.2 -> Nullstelle 
          */
-    	rechnen[4] = new Tabelle(-2 , 0.00000001 , 2 , -20 , -6, 30);
-    	rechnen[4].newton(-2);
-    	rechnen[5] = new Tabelle(1 , 0.00000001 , 2 , -20 , -6, 30);
-    	rechnen[5].newton(1);
-    	rechnen[6] = new Tabelle(50 , 0.00000001 , 2 , -20 , -6, 30);
-    	rechnen[6].newton(50);
+        rechnen[4] = new Tabelle(-2 , 0.00000001 , 2 , -20 , -6, 30);
+        rechnen[4].newton(-2);
+        rechnen[5] = new Tabelle(1 , 0.00000001 , 2 , -20 , -6, 30);
+        rechnen[5].newton(1);
+        rechnen[6] = new Tabelle(50 , 0.00000001 , 2 , -20 , -6, 30);
+        rechnen[6].newton(50);
     } 
 }
 
@@ -91,7 +91,7 @@ class Parabel extends Funktion {
         return this.x; 
     }
     public double e() {
-    	return eps;
+        return eps;
     }
 }
 
@@ -112,13 +112,13 @@ class Tabelle extends Funktion {
         return this.a * x * x * x + this.b * x * x + this.c * x + this.d;
     }
     public double g() {
-    	return this.a * 3 * x * x +this.b * 2 * x + this.c;
+        return this.a * 3 * x * x + this.b * 2 * x + this.c;
     }
     public double t() {
         return x;
     }
     public double e() {
-    	return eps;
+        return eps;
     }
 }
 
