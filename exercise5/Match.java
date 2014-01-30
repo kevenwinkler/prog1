be2e64efbf2a1287afbd0f72521fe16652be7c17
Tobias Stein <tobias.stein@tu-bs.de>
2014-01-30 14:30:40 +0100

    Erste funktionierende Lösung ohne Rekursion und ohne Berücksichtigung der Formatierung

--- exercise5/Match.java
+++ exercise5/Match.java
@@ -26,9 +26,80 @@ public class Match {
 					}
 			}
 		}
+		match(p,s);
 	}
 	
-	public static boolean match(String p, String s ) {
-		return false;
+	public static boolean match(String p, String s) {
+		boolean value = false;
+		int t = 0;
+		int v = 0;
+		int stern = 0;
+		int diff = p.length()-s.length();
+		//System.out.println(diff);
+		int laenge = Math.max(p.length(),s.length());
+		//System.out.println("max. Laenge: "+ laenge);
+		for (int i=0; i < p.length(); i++) {
+			if (p.charAt(i) == '*') {
+				stern++;
+			}
+		}
+		// Array anlegen, welches nur die Zahl 1 enthält
+		int[] intArray = new int[laenge]; 
+		for (int z = 0; z < laenge; z++) {
+			intArray[z] = 1;
+		}
+		if (p.length()-stern > s.length()) {
+			System.out.println(value);
+			return value;
+		}
+		System.out.println("----");
+		while (t < 500) {
+			System.out.print("D " + v + ": ");
+			try {
+				if (p.substring(v,v+1).equals(s.substring(t,t+1))) {
+					System.out.println("Vergleiche String: " + p.substring(v,v+1)+ " und " + s.substring(t,t+1) );
+					intArray[t] = 1;
+					System.out.println("Ergebnis: "+ intArray[t]);
+					t++;
+					v++;
+					continue;
+				} else if (p.charAt(v) == '*') {
+					System.out.println("Vergleiche String: Joker mit " + s.substring(t,t+1) );
+					intArray[t] = 1;
+					System.out.println("Ergebnis: "+ intArray[t]);
+					while (p.charAt(v) == '*') {
+						v++;
+						System.out.println("Do String: " + p.substring(v,v+1)+ " und " + s.substring(t,t+1) );
+					} 
+					while (!(p.substring(v,v+1).equals(s.substring(t,t+1)))) {
+						t++;
+						System.out.println("Do String: " + p.substring(v,v+1)+ " und " + s.substring(t,t+1) );
+					}
+				} else {
+					System.out.println("Vergleiche String: "+ p.substring(v,v+1)+ " und " + s.substring(t,t+1) );
+					intArray[t] = 0;
+					System.out.println("Ergebnis: " + intArray[t]);
+					t++;
+					v++;
+					continue;
+				}
+			}
+			catch (StringIndexOutOfBoundsException ex) {
+				System.out.println("IndexOutOfBounds!");
+				break;
+			}
+		}
+		for (int z = 0; z < laenge; z++) {
+			if (intArray[z] == 0) {
+				value = false;
+				break;
+			} else {
+				value = true;
+				continue;
+			}
+		}
+	System.out.println("----");
+	System.out.println(value);
+	return value;
 	}
 }
